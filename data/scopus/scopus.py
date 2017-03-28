f01814de8bcb6f47b1826cb116b30c6e08fa9564
t00n <kelkununtel@hotmail.com>
2017-03-28 12:33:05 +0200

    [add] cache requests

--- data/scopus/scopus.py
+++ data/scopus/scopus.py
@@ -1,4 +1,9 @@
 import requests
+import json
+from functools import wraps
+
+CACHE_FILE = '.cache'
+API_KEY = 'a52b0c7edc190b35f2740c8bde849893'
 
 proxies = {
     'http': 'socks5://localhost:22080',
@@ -8,13 +13,39 @@ proxies = {
 SEARCH_URL = 'https://api.elsevier.com/content/search/scopus'
 
 
+def tuplize(something):
+    if isinstance(something, list) or isinstance(something, tuple):
+        something = tuple([tuplize(elem) for elem in something])
+    elif isinstance(something, dict):
+        something = tuple([(tuplize(k), tuplize(v)) for k, v in something.items()])
+    return something
+
+
+def cache(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = str(tuplize(args) + tuplize(kwargs))
+        try:
+            with open(CACHE_FILE, 'r') as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            data = {}
+        if key not in data:
+            data[key] = func(*args, **kwargs)
+            with open(CACHE_FILE, 'w') as f:
+                json.dump(data, f)
+        return data[key]
+    return wrapper
+
+
+@cache
 def requests_get(*args, **kwargs):
     if 'params' in kwargs:
         params = kwargs['params']
     else:
         params = {}
-    params['apiKey'] = '7f59af901d2d86f78a1fd60c1bf9426a'
-    return requests.get(*args, **kwargs, proxies=proxies)
+    params['apiKey'] = API_KEY
+    return requests.get(*args, **kwargs, proxies=proxies).text
 
 
 def scopus_search_by_title(title):
@@ -26,4 +57,4 @@ def scopus_search_by_title(title):
 
 if __name__ == '__main__':
     response = scopus_search_by_title('a survey on reactive programming')
-    print(response.json()['search-results']['entry'])
+    print(json.loads(response)['search-results']['entry'])
