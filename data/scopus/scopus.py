1b0086444a005138366fcab4efe5e62eef97c63d
t00n <kelkununtel@hotmail.com>
2017-03-28 13:21:17 +0200

    [add] get citing papers and full metadata

--- data/scopus/scopus.py
+++ data/scopus/scopus.py
@@ -5,7 +5,8 @@ import os
 import hashlib
 
 CACHE_DIR = '.cache'
-API_KEY = 'a52b0c7edc190b35f2740c8bde849893'
+# API_KEY = 'a52b0c7edc190b35f2740c8bde849893'
+API_KEY = '7f59af901d2d86f78a1fd60c1bf9426a'
 
 proxies = {
     'http': 'socks5://localhost:22080',
@@ -13,6 +14,11 @@ proxies = {
 }
 
 SEARCH_URL = 'https://api.elsevier.com/content/search/scopus'
+SIMPLE_METADATA_URL = 'https://api.elsevier.com/content/search/scopus'
+FULL_METADATA_URL = 'https://api.elsevier.com/content/abstract/scopus_id/{}'
+
+if not os.path.exists(CACHE_DIR):
+    os.mkdir(CACHE_DIR)
 
 
 def tuplize(something):
@@ -26,7 +32,8 @@ def tuplize(something):
 def cache(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        key = hashlib.sha224(str(tuplize(args) + tuplize(kwargs)).encode('utf-8')).hexdigest()
+        key = API_KEY + str(tuplize(args) + tuplize(kwargs))
+        key = hashlib.sha224(key.encode('utf-8')).hexdigest()
         try:
             with open(os.path.join(CACHE_DIR, key), 'r') as f:
                 data = json.load(f)
@@ -42,19 +49,49 @@ def cache(func):
 def requests_get(*args, **kwargs):
     if 'params' in kwargs:
         params = kwargs['params']
+        del kwargs['params']
     else:
         params = {}
     params['apiKey'] = API_KEY
-    return requests.get(*args, **kwargs, proxies=proxies).text
+    return requests.get(*args, **kwargs, params=params, proxies=proxies).text
 
 
 def scopus_search_by_title(title):
     params = {
         'query': 'TITLE-ABS-KEY ( "{}" ) '.format(title)
     }
-    return requests_get(SEARCH_URL, params=params)
+    return json.loads(requests_get(SEARCH_URL, params=params))
 
+
+def scopus_get_simple_metadata_by_eid(eid):
+    params = {
+        'query': 'EID({})'.format(eid),
+        'httpAccept': 'application/json'
+    }
+    return json.loads(requests_get(SIMPLE_METADATA_URL, params=params))
+
+
+def scopus_get_citing_papers_by_eid(eid):
+    params = {
+        'query': 'REFEID({})'.format(eid),
+        'httpAccept': 'application/json'
+    }
+    return json.loads(requests_get(SIMPLE_METADATA_URL, params=params))
+
+
+def scopus_get_full_metadata_by_eid(eid):
+    simple_metadata = scopus_get_simple_metadata_by_eid(eid)
+    sid = simple_metadata['search-results']['entry'][0]['dc:identifier'].split(':')[-1]
+    params = {
+        'httpAccept': 'application/json'
+    }
+    return requests_get(FULL_METADATA_URL.format(sid), params=params)
 
 if __name__ == '__main__':
-    response = scopus_search_by_title('a survey on reactive programming')
-    print(json.loads(response)['search-results']['entry'])
+    # response = scopus_search_by_title('a survey on reactive programming')
+    # print(json.loads(response)['search-results']['entry'])
+    # 2-s2.0-84891048418
+    # response = scopus_get_simple_metadata_by_eid('2-s2.0-84891048418')
+    # print(response)
+    response = scopus_get_full_metadata_by_eid('2-s2.0-84891048418')
+    print(response)
