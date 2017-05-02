91a210b07facafde70c06b80b5e284fde2afe390
maribib <maribib.cerkinfo@gmail.com>
2017-05-02 09:20:00 +0200

    [ref] hardcoded to function

--- django/static/js/d3-fun.js
+++ django/static/js/d3-fun.js
@@ -53,11 +53,10 @@ var data = {
    }]
  };
 
-var c10 = d3.scale.category10();
 var svg = d3.select("#graph-container svg");
 
 svg.append('svg:defs').append('svg:marker')
-.attr('id', 'mid-arrow')
+.attr('id', 'mid-arrow-left')
 .attr('viewBox', '0 -5 10 10')
 .attr('refX', 4)
 .attr('markerWidth', 10)
@@ -67,199 +66,114 @@ svg.append('svg:defs').append('svg:marker')
 .attr('d', 'M10,-5L0,0L10,5L6,0')
 .attr('fill', '#000');
 
-var container_width = parseInt(window.getComputedStyle(svg[0][0], null).getPropertyValue("width"));
-var container_height = parseInt(window.getComputedStyle(svg[0][0], null).getPropertyValue("height"));
-var SIDEBAR_OFFSET = parseInt(window.getComputedStyle($("search")[0], null).getPropertyValue("width"));
-var mid_width = container_width/2;
-var mid_height = container_height/2;
-var dot_radius = 40;
-var dot_space = 100;
-var col_offset = 400;
-
-var left_column_offset = ((container_height-((data.prev.length-1)*dot_space)-dot_radius*2)/2)-(dot_radius*1.5);
-var right_column_offset = ((container_height-((data.next.length-1)*dot_space)-dot_radius*2)/2)-(dot_radius*1.5);
+svg.append('svg:defs').append('svg:marker')
+.attr('id', 'mid-arrow-right')
+.attr('viewBox', '0 -5 10 10')
+.attr('refX', 4)
+.attr('markerWidth', 10)
+.attr('markerHeight', 10)
+.attr('orient', 'auto')
+.append('svg:path')
+.attr('d', 'M0,-5L10,0L0,5L5,0')
+.attr('fill', '#000');
 
 var tooltip = d3.select("body").append("div")   
 .attr("class", "tooltip")               
 .style("opacity", 0);
 
-var y = left_column_offset;
-var links = svg.selectAll("link")
-.data(data.prev)
-.enter()
-.append("line")
-.attr("class", "link")
-.attr("x1", function(l) {
-    y = y+dot_space;
-    d3.select(this).attr("y1", y);
-    return mid_width-col_offset;
-})
-.attr("x2", function(l) {
-    const x1 = parseInt(d3.select(this).attr("x1"));
-    const y1 = parseInt(d3.select(this).attr("y1"));
-    d3.select(this).attr("y2", y1+(mid_height-y1)/2);
-    return x1+(col_offset/2);
-})
-.attr("fill", "none")
-.attr("stroke", "black")
-.attr("marker-end", 'url(#mid-arrow)');
+function getComputedProperty(obj, property) {
+    return parseInt(window.getComputedStyle(obj, null).getPropertyValue(property));
+}
 
-var y = left_column_offset;
-var links = svg.selectAll("link")
-.data(data.prev)
-.enter()
-.append("line")
-.attr("class", "link")
-.attr("x1", function(l) {
-    y = y+dot_space;
-    d3.select(this).attr("y1", y+(mid_height-y)/2);
-    return mid_width-(col_offset/2);
-})
-.attr("x2", function(l) {
-    d3.select(this).attr("y2", mid_height);
-    return mid_width;
-})
-.attr("fill", "none")
-.attr("stroke", "black")
+const DOT_RADIUS = 40;
+const DOT_SPACE = 100;
+const COL_OFFSET = 400;
 
-var y = right_column_offset;
-var links = svg.selectAll("link")
-.data(data.next)
-.enter()
-.append("line")
-.attr("class", "link")
-.attr("x1", function(l) {
-    d3.select(this).attr("y1", mid_height);
-    return mid_width;
-})
-.attr("x2", function(l) {
-    y = y + dot_space;
-    d3.select(this).attr("y2", mid_height+(mid_height-y)/2);
-    return mid_width+(col_offset/2);
-})
-.attr("fill", "none")
-.attr("stroke", "black")
-.attr("marker-end", 'url(#mid-arrow)');
+var container_width = getComputedProperty($("svg")[0], "width");
+var container_height = getComputedProperty($("svg")[0], "height");
+var sidebar_offset = getComputedProperty($("search")[0], "width");
+var mid_width = container_width/2;
+var mid_height = container_height/2;
+var mid = {x: mid_width, y: mid_height};
 
-var y = right_column_offset;
-var links = svg.selectAll("link")
-.data(data.next)
-.enter()
-.append("line")
-.attr("class", "link")
-.attr("x2", function(l) {
-    y = y+dot_space;
-    d3.select(this).attr("y2", y);
-    return mid_width+col_offset;
-})
-.attr("x1", function(l) {
-    const y2 = parseInt(d3.select(this).attr("y2"));
-    d3.select(this).attr("y1", mid_height-(mid_height-y2)/2);
-    return mid_width+(col_offset/2);
-})
-.attr("fill", "none")
-.attr("stroke", "black")
+var left_column_offset = ((container_height-((data.prev.length-1)*DOT_SPACE)-DOT_RADIUS*2)/2)-(DOT_RADIUS*1.5);
+var right_column_offset = ((container_height-((data.next.length-1)*DOT_SPACE)-DOT_RADIUS*2)/2)-(DOT_RADIUS*1.5);
+
+
+function draw_link(frm, to, arrow) {
+    const mid_x = frm.x + (to.x-frm.x)/2;
+    const mid_y = frm.y + (to.y-frm.y)/2;
+    svg.append("line")
+    .attr("class", "link")
+    .attr("x1", frm.x)
+    .attr("y1", frm.y)
+    .attr("x2", mid_x)
+    .attr("y2", mid_y)
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("marker-end", arrow);
+    svg.append("line")
+    .attr("class", "link")
+    .attr("x1", mid_x)
+    .attr("y1", mid_y)
+    .attr("x2", to.x)
+    .attr("y2", to.y)
+    .attr("fill", "none")
+    .attr("stroke", "black");
+}
+
+function draw_links(nb_links, x, y, arrow) {
+    for (var i=0; i<nb_links; ++i) {
+        y += DOT_SPACE;
+        draw_link({x:x,y:y}, mid, arrow)
+    }
+}
 
 // NODES
 
-var y = left_column_offset;
-svg.selectAll("node")
-.data(data.prev)
-.enter()
-.append("circle")
-.attr("class", "prev-node node")
-.attr("cx", mid_width-col_offset)
-.attr("cy", function(d) {
-    y = y+dot_space;
-    return y
-}.bind(y))
-.attr("r", dot_radius)
-.attr("fill", COLOR_PREV)
-.attr("stroke", "black")
-.attr("stroke-width", CIRCLE_STROKE)
-.on({
-    "mouseover": function(d) {  
-        d3.select(this).style("cursor", "pointer");    
-        tooltip.transition()        
-            .duration(200)      
-            .style("opacity", .95);      
-        tooltip.html("<b>"+d.name+"</b><hr>"+d.author + "<br>"+d.year +'<hr><span class="tag prev">'+ d.keywords.split(", ").join('</span><span class="tag prev">')+"</span>")  
-            .style("left", (d3.select(this).attr("cx") - $(tooltip[0][0]).width()/2 + SIDEBAR_OFFSET) + "px")     
-            .style("top", (d3.select(this).attr("cy") - $(tooltip[0][0]).height()) + "px");    
-    },
-    "mouseout": function(d) {    
-        d3.select(this).style("cursor", "default"); 
-        tooltip.transition()        
-            .duration(500)      
-            .style("opacity", 0);   
-    }
-}); 
+function draw_nodes(datas, x, y, color, type) {
+    var circle = svg.selectAll(type)
+    .data(datas)
+    .enter()
+    .append("circle")
+    .attr("class", "node "+type)
+    .attr("cx", x)
+    .attr("cy", function(d) {
+        y += DOT_SPACE;
+        return y;
+    })
+    .attr("r", DOT_RADIUS)
+    .attr("fill", color)
+    .attr("stroke", "black")
+    .attr("stroke-width", CIRCLE_STROKE)
+    .on({
+        "mouseover": function(d) {  
+            d3.select(this).style("cursor", "pointer");    
+            tooltip.transition()        
+                .duration(200)      
+                .style("opacity", .95);      
+            tooltip.html("<b>"+d.name+"</b><hr>"+d.author + "<br>"+d.year +'<hr><span class="tag '+type+'">'+ d.keywords.split(", ").join('</span><span class="tag '+type+'">')+"</span>")  
+                .style("left", (d3.select(this).attr("cx") - $(tooltip[0][0]).width()/2 + sidebar_offset) + "px")     
+                .style("top", (d3.select(this).attr("cy") -1.5*DOT_RADIUS - $(tooltip[0][0]).height()) + "px");    
+        },
+        "mouseout": function() {    
+            d3.select(this).style("cursor", "default"); 
+            tooltip.transition()        
+                .duration(500)      
+                .style("opacity", 0);   
+        }
+    });
+}
 
-svg.selectAll("node")
-.data(data.curr)
-.enter()
-.append("circle")
-.attr("class", "curr-node node")
-.attr("cx", mid_width)
-.attr("cy", mid_height)
-.attr("r", dot_radius)
-.attr("fill", COLOR_CURR)
-.attr("stroke", "black")
-.attr("stroke-width", CIRCLE_STROKE)
-.on({
-    "click": function(d) {
-        var speed = 750;
-        $('html, body').animate( { scrollTop: $("#map-container").offset().top }, speed ); 
-    },
-    "mouseover": function(d) {
-        d3.select(this).style("cursor", "pointer");
-        tooltip.transition()        
-            .duration(200)      
-            .style("opacity", .95);      
-        tooltip .html("<b>"+d.name+"</b><hr>"+d.author + "<br>"+d.year +'<hr><span class="tag curr">'+ d.keywords.split(", ").join('</span><span class="tag curr">')+"</span>")  
-            .style("left", (d3.select(this).attr("cx") - $(tooltip[0][0]).width()/2) + SIDEBAR_OFFSET + "px")     
-            .style("top", (d3.select(this).attr("cy") - $(tooltip[0][0]).height()) + "px");    
-    },
-    "mouseout": function(d) {
-        d3.select(this).style("cursor", "default");
-        tooltip.transition()        
-            .duration(500)      
-            .style("opacity", 0);
-    }
-});
+function draw_scene() {
+    draw_links(data.prev.length, mid_width-COL_OFFSET, left_column_offset, "url(#mid-arrow-left)");
+    draw_links(data.next.length, mid_width+COL_OFFSET, right_column_offset, "url(#mid-arrow-right)")
+    draw_nodes(data.prev, mid_width-COL_OFFSET, left_column_offset, COLOR_PREV, "prev");
+    draw_nodes(data.curr, mid_width, mid_height-DOT_SPACE, COLOR_CURR, "curr");
+    draw_nodes(data.next, mid_width+COL_OFFSET, right_column_offset, COLOR_NEXT, "next");
+}
 
-var y = right_column_offset;
-svg.selectAll("node")
-.data(data.next)
-.enter()
-.append("circle")
-.attr("class", "next-node node")
-.attr("cx", mid_width+col_offset)
-.attr("cy", function(d) {
-    y = y+dot_space;
-    return y
-})
-.attr("r", dot_radius)
-.attr("fill", COLOR_NEXT)
-.attr("stroke", "black")
-.attr("stroke-width", CIRCLE_STROKE)
-.on({
-    "mouseover": function(d) {  
-        d3.select(this).style("cursor", "pointer");    
-        tooltip.transition()        
-            .duration(200)      
-            .style("opacity", .95);      
-        tooltip .html("<b>"+d.name+"</b><hr>"+d.author + "<br>"+d.year +'<hr><span class="tag next">'+ d.keywords.split(", ").join('</span><span class="tag next">')+"</span>")
-            .style("left", (d3.select(this).attr("cx") - $(tooltip[0][0]).width()/2) + SIDEBAR_OFFSET + "px")     
-            .style("top", (d3.select(this).attr("cy") - $(tooltip[0][0]).height()) + "px");    
-    },
-    "mouseout": function(d) {    
-        d3.select(this).style("cursor", "default"); 
-        tooltip.transition()        
-            .duration(500)      
-            .style("opacity", 0);   
-    }
-});
+draw_scene();
 
 //#### NAV ####
 
@@ -276,136 +190,86 @@ var nav_text = d3.select("body").append("div")
 .attr("class", "nav_text")
 .style("opacity", 0);
 
-svg
-.append("line")
-.style("stroke", "black")
-.style("stroke-width", NAV_STROKE_WIDTH)
-.attr("x1", mid_width-(NAV_LENGTH/2))
-.attr("y1", container_height-NAV_OFFSET)
-.attr("x2", mid_width+(NAV_LENGTH/2))
-.attr("y2", container_height-NAV_OFFSET);
-
-svg
-.append("line")
-.style("stroke", "black")
-.style("stroke-width", NAV_STROKE_WIDTH)
-.style("stroke-dasharray", ("3, 3"))
-.attr("x1", mid_width-(NAV_LENGTH/2)-NAV_EXTREMITY)
-.attr("y1", container_height-NAV_OFFSET)
-.attr("x2", mid_width-(NAV_LENGTH/2))
-.attr("y2", container_height-NAV_OFFSET);
-
-svg
-.append("line")
-.style("stroke", "black")
-.style("stroke-dasharray", ("3, 3"))
-.style("stroke-width", NAV_STROKE_WIDTH)
-.attr("x1", mid_width+(NAV_LENGTH/2))
-.attr("y1", container_height-NAV_OFFSET)
-.attr("x2", mid_width+(NAV_LENGTH/2)+NAV_EXTREMITY)
-.attr("y2", container_height-NAV_OFFSET);
-
-svg.append("circle")
-.attr("cx", mid_width)
-.attr("cy", container_height-NAV_OFFSET)
-.attr("r", NAV_DOT_RADIUS)
-.attr("fill", COLOR_CURR)
-.attr("stroke", "black")
-.attr("stroke-width", NAV_DOT_STROKE_WIDTH)
-.on({
-    "mouseover": function(d) {
-        nav_text.transition()        
-            .duration(200)      
-            .style("opacity", .95);  
-        d3.select(this).style("cursor", "pointer");   
-        nav_text.html("Currently displayed article.")
-        .style("left", (d3.select(this).attr("cx") - $(nav_text[0][0]).width()/2) + SIDEBAR_OFFSET + "px")     
-        .style("top", (container_height-15) + "px"); 
-    },
-    "mouseout": function(d) {
-        d3.select(this).style("cursor", "default"); 
-        nav_text.transition()        
-        .duration(500)      
-        .style("opacity", 0);  
+function draw_line(x1, y1, x2, y2, type) {
+    var line = svg.append("line")
+    .style("stroke", "black")
+    .style("stroke-width", NAV_STROKE_WIDTH)
+    .attr("x1", x1)
+    .attr("y1", y1)
+    .attr("x2", x2)
+    .attr("y2", y2);
+    if (type=="dotted") {
+        line.style("stroke-dasharray", ("3, 3"));
     }
-});
+};
 
-for (var i=1; i<NAV_HISTORY_SIZE+1; ++i) {
-    svg.append("circle")
-    .attr("cx", mid_width-(i*NAV_DOT_SPACE))
-    .attr("cy", container_height-NAV_OFFSET)
+function draw_nav_nodes(datas) {
+    svg.selectAll("nav-node")
+    .data(datas)
+    .enter()
+    .append("circle")
+    .attr("class", "nav-node")
+    .attr("cx", function(d) { return d.x })
+    .attr("cy", function(d) { return d.y })
     .attr("r", NAV_DOT_RADIUS)
-    .attr("fill", (i==1 ? COLOR_PREV : COLOR_PRIMARY))
+    .attr("fill", function(d) { return d.color })
     .attr("stroke", "black")
     .attr("stroke-width", NAV_DOT_STROKE_WIDTH)
     .on({
         "mouseover": function(d) {
             nav_text.transition()        
-            .duration(200)      
-            .style("opacity", .95);   
-            d3.select(this).style("cursor", "pointer");
-            nav_text.html("Navigate to the th node on the path to a founding article.")
-            .style("left", (d3.select(this).attr("cx") - $(nav_text[0][0]).width()/2) + SIDEBAR_OFFSET + "px")  
-            .style("top", (container_height-15) + "px");
+                .duration(200)      
+                .style("opacity", .95);  
+            d3.select(this).style("cursor", "pointer");   
+            nav_text.html(d.text)
+            .style("left", (d3.select(this).attr("cx") - $(nav_text[0][0]).width()/2) + sidebar_offset + "px")     
+            .style("top", (container_height-NAV_OFFSET-30) + "px"); 
         },
         "mouseout": function(d) {
-            d3.select(this).style("cursor", "default");
+            d3.select(this).style("cursor", "default"); 
             nav_text.transition()        
             .duration(500)      
-            .style("opacity", 0);
+            .style("opacity", 0);  
         }
-    });
+    })
 }
 
-svg.append("circle")
-.attr("cx", mid_width+NAV_DOT_SPACE)
-.attr("cy", container_height-NAV_OFFSET)
-.attr("r", NAV_DOT_RADIUS)
-.attr("fill", COLOR_NEXT)
-.attr("stroke", "black")
-.attr("stroke-width", NAV_DOT_STROKE_WIDTH)
-.on({
-    "mouseover": function(d) {
-        nav_text.transition()        
-        .duration(200)      
-        .style("opacity", .95);
-        d3.select(this).style("cursor", "pointer");
-        nav_text.html("Navigate randomly to an article that reference the selected article.")
-        .style("left", (d3.select(this).attr("cx") - $(nav_text[0][0]).width()/2) + SIDEBAR_OFFSET + "px")  
-        .style("top", (container_height-15) + "px");
-    },
-    "mouseout": function(d) {
-        d3.select(this).style("cursor", "default");
-        nav_text.transition()        
-        .duration(500)      
-        .style("opacity", 0); 
+function draw_nav() {
+    const mid1_x = mid_width-(NAV_LENGTH/2);
+    const mid2_x = mid_width+(NAV_LENGTH/2);
+    const y = container_height-NAV_OFFSET;
+    draw_line(mid1_x-NAV_EXTREMITY, y, mid1_x, y, "dotted");
+    draw_line(mid1_x, y, mid2_x, y);
+    draw_line(mid2_x, y, mid2_x+NAV_EXTREMITY, y, "dotted");
+    const nav_datas = [
+        { 
+            text: "Currently displayed article.",
+            x: mid_width,
+            y: y,
+            color: COLOR_CURR
+        }, {
+            text: "Navigate randomly to an article that reference the selected article.",
+            x: mid_width+NAV_DOT_SPACE,
+            y: y,
+            color: COLOR_NEXT
+        }, {
+            text: "Navigate to a founding article.",
+            x: mid1_x-NAV_EXTREMITY+5,
+            y: y,
+            color: COLOR_PRIMARY,
+        }
+    ];
+    for (var i=1; i<NAV_HISTORY_SIZE+1; ++i) {
+        nav_datas.push({
+            text: "Navigate to the "+i+"th node on the path to a founding article.",
+            x: mid_width-(i*NAV_DOT_SPACE),
+            y: y,
+            color: (i==1 ? COLOR_PREV : COLOR_PRIMARY)
+        });
     }
-});
-
-svg.append("circle")
-.attr("cx", mid_width-(NAV_LENGTH/2)-NAV_EXTREMITY+5)
-.attr("cy", container_height-NAV_OFFSET)
-.attr("r", NAV_DOT_RADIUS)
-.attr("fill", COLOR_PRIMARY)
-.attr("stroke", "black")
-.attr("stroke-width", NAV_DOT_STROKE_WIDTH)
-.on({
-    "mouseover": function(d) {
-        nav_text.transition()        
-        .duration(200)      
-        .style("opacity", .95);
-        d3.select(this).style("cursor", "pointer");
-        nav_text.html("Navigate to a founding article.")
-        .style("left", (d3.select(this).attr("cx") - $(nav_text[0][0]).width()/2) + SIDEBAR_OFFSET + "px")  
-        .style("top", (container_height-15) + "px");
-    },
-    "mouseout": function(d) {
-        nav_text.transition()        
-        .duration(500)      
-        .style("opacity", 0); 
-        d3.select(this).style("cursor", "default");
-    }
-});
+    draw_nav_nodes(nav_datas);
+}
+draw_nav();
 
 //#### LEGEND ####
 
@@ -414,26 +278,13 @@ const LEGEND_H_OFFSET = 30
 const LEGEND_V_OFFSET = 50
 const LEGEND_TEXT_OFFSET = 10
 
-svg
-.append("line")
-.attr("class", "legend-line")
-.attr("x1", container_width-LEGEND_LENGTH-LEGEND_H_OFFSET)
-.attr("y1", container_height-LEGEND_V_OFFSET)
-.attr("x2", container_width-(LEGEND_LENGTH/2)-LEGEND_H_OFFSET)
-.attr("y2", container_height-LEGEND_V_OFFSET)
-.attr("fill", "none")
-.attr("stroke", "black")
-.attr("marker-end", 'url(#mid-arrow)');
-
-svg
-.append("line")
-.attr("class", "legend-line")
-.attr("x1", container_width-(LEGEND_LENGTH/2)-LEGEND_H_OFFSET)
-.attr("y1", container_height-LEGEND_V_OFFSET)
-.attr("x2", container_width-LEGEND_H_OFFSET)
-.attr("y2", container_height-LEGEND_V_OFFSET)
-.attr("fill", "none")
-.attr("stroke", "black")
+const legend_x = container_width-LEGEND_H_OFFSET;
+const legend_y = container_height-LEGEND_V_OFFSET;
+draw_link(
+    { x: legend_x-LEGEND_LENGTH, y: legend_y }, 
+    { x: legend_x, y: legend_y }, 
+    'url(#mid-arrow-left)'
+)
 
 svg.append("text")
 .attr("x", container_width-LEGEND_LENGTH-LEGEND_H_OFFSET)
