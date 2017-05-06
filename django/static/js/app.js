6636cf6609220c6f9de30bdc1a4754ff0134041d
t00n <kelkununtel@hotmail.com>
2017-05-06 20:22:53 +0200

    [enh] little hack to make the data retrieval faster

--- django/static/js/app.js
+++ django/static/js/app.js
@@ -23,33 +23,17 @@ function get_by_sid(sid, onSuccess, onError) {
 }
 
 function get_prev(metadata, onSuccess, onError) {
-    function lol(res, i) {
-        if (metadata.prev[i]) {
-            $.ajax({
-                url: metadata.prev[i].url,
-                success: (data) => lol(res.concat([data]), i + 1),
-                error: (error) => lol(res, i + 1)
-            })
-        }
-        else {
-            onSuccess(res)
-        }
-    }
-    lol([], 0)
+    $.ajax({
+        url: "pomme_de_prev?title=" + metadata.title,
+        success: (data) => onSuccess(data.prev),
+        error: onError
+    })
 }
 
 function get_next(metadata, onSuccess, onError) {
-    function lol(res, i) {
-        if (metadata.next[i]) {
-            $.ajax({
-                url: metadata.next[i].url,
-                success: (data) => lol(res.concat([data]), i + 1),
-                error: (error) => lol(res, i + 1)
-            })
-        }
-        else {
-            onSuccess(res)
-        }
-    }
-    lol([], 0)
+    $.ajax({
+        url: "pomme_de_next?title=" + metadata.title,
+        success: (data) => onSuccess(data.next),
+        error: onError
+    })
 }
